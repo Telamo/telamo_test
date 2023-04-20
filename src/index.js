05c441a43feb722d9e733f8e08384cac02920db4
Telamo <congdanh2205@gmail.com>
2023-04-20 15:18:55 +0700

    dsf

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const port = 8000
 const rootRoute = require('./routes/rootroute')
 const app = express()
 
+app.use(express.json())
 app.listen(port, () => {
     console.log('App listening on port: ' + port)
 })
 
-app.use('/', rootRoute)
+app.use('/api', rootRoute)
